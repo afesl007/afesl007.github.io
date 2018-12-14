d41a053be65202b304cb5e235baa99337b8a5dc5
afesl007 <43017162+afesl007@users.noreply.github.com>
2018-12-13 19:19:22 -0800

    Web Scrapping file
    The file gets content from a webpage and send it to a user a message.

---
+++ webScrapingMessage.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+from twilio.rest import Client
+
+url = 'https://www.google.com/'
+# Your Account SID from twilio.com/console
+account_sid = "######################"
+# Your Auth Token from twilio.com/console
+auth_token  = "########################"
+
+twilio_phone_number = '+12028310725'
+my_phone_number = '+12026044570'
+
+webpage = requests.get(url)
+soup = BeautifulSoup(webpage.text, 'html.parser')
+
+free_food = [s for s in soup.body.stripped_strings if 'free' in s.lower()]
+
+body = 'Free Postmates!\n\n' + '\n'.join(free_food)
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+12026044570",
+    from_="+12028310725",
+    body=body
+)
+
+print(message.sid)
+
